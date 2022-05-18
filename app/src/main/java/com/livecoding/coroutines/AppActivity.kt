9980c99908e913ce350c7c1320a5b07f5bb2be06
Nikita Yatskivskiy <nikrestinpiece@gmail.com>
2022-05-18 13:08:08 +0300

    Initial commit

---
+++ app/src/main/java/com/livecoding/coroutines/AppActivity.kt
@@ -0,0 +1,12 @@
+package com.livecoding.coroutines
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+}
